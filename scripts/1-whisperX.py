924af46707a4e137e02de56d74f7fe924d51b271
Maxine Badiola <maxine_badiola@dlsu.edu.ph>
2025-06-27 02:36:44 +0800

    FIX: variables

--- scripts/1-whisperX.py
+++ scripts/1-whisperX.py
@@ -14,7 +14,7 @@ import torch
 import whisperx
 from pathlib import Path
 
-audioFile = "audio/15M_audio.wav"
+audioFile = "audio/31M_audio.wav"
 outputTxt = "output/transcript.txt"
 outputTimestamps = "output/timestamps.txt"
 
@@ -23,8 +23,8 @@ model = whisperx.load_model("large-v2", device="cuda", compute_type="float16")
 print(f"Transcribing... ({audioFile})")
 result = model.transcribe(audioFile, language="en")
 
-modelA, metadata = whisperx.load_align_model(language_code="en", device=device)
-alignedResult = whisperx.align(result["segments"], modelA, metadata, audioFile, device)
+modelA, metadata = whisperx.load_align_model(language_code="en", device="cuda")
+alignedResult = whisperx.align(result["segments"], modelA, metadata, audioFile, "cuda")
 
 with open(outputTxt, "w", encoding="utf-8") as f:
     fullText = " ".join(segment["text"].strip() for segment in alignedResult["segments"])
